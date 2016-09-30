65623039af1659b952e6aa1a06c479730b9190d1
gou <gou@GOUDT>
2016-09-30 11:18:34 -0700

    Add project files.

---
+++ ImageProcessingTesting/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+
+namespace ImageProcessingTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String win1 = "Test Window";
+            CvInvoke.NamedWindow(win1);
+            Mat img = new Mat(200, 400, DepthType.Cv8U, 3);
+            img.SetTo(new Bgr(255, 0, 0).MCvScalar);
+
+            CvInvoke.PutText(
+                img,
+                "Hello, world",
+                new System.Drawing.Point(10, 80),
+                FontFace.HersheyComplex,
+                1.0,
+                new Bgr(0, 255, 255).MCvScalar);
+            CvInvoke.Imshow(win1, img); //Show the image
+            CvInvoke.WaitKey(0);  //Wait for the key pressing event
+            CvInvoke.DestroyWindow(win1); //Destroy the window if key is pressed
+
+        }
+    }
+}
